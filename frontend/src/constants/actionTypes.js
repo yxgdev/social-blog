af52e559d2123682ff89676b7dec1737c6e867eb
yxgdev <yeexuan73@gmail.com>
2022-01-21 17:31:22 +0800

    added SIGN_IN action types

--- frontend/src/constants/actionTypes.js
+++ frontend/src/constants/actionTypes.js
@@ -5,3 +5,4 @@ export const UPDATE_POST_VIEWS = "UPDATE_POST_VIEWS";
 export const GET_SINGLE_POST = "GET_SINGLE_POST";
 export const AUTH = "AUTH";
 export const SIGN_OUT = "SIGN_OUT";
+export const SIGN_IN = "SIGN_IN";
